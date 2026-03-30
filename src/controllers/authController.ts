7e6aa60bbd43743e78693a2c4dbdf3c225772d17
kishanpp <kish101993@gmail.com>
2026-03-30 15:50:39 +0530

    feat: added global error handler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,19 +2,15 @@ import type { Request, Response } from 'express';
 import * as authService from '../services/authService.js';
 
 export const loginUser = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { username, password } = req.body;
-    if (username === undefined || password === undefined) {
-      res.status(400).json({ message: 'Username and password are required' });
-      return;
-    }
-    const token = await authService.loginUser(username, password);
-    if (!token) {
-      res.status(401).json({ message: 'Invalid credentials' });
-      return;
-    }
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Error logging in', error });
+  const { username, password } = req.body;
+  if (username === undefined || password === undefined) {
+    res.status(400).json({ message: 'Username and password are required' });
+    return;
   }
+  const token = await authService.loginUser(username, password);
+  if (!token) {
+    res.status(401).json({ message: 'Invalid credentials' });
+    return;
+  }
+  res.status(200).json({ token });
 };
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,32 @@
-import express from "express";
-import cors from "cors";
-import { createServer } from "http";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import dotenv from 'dotenv';
+import errorHandler from './middleware/errorHandler.js';
 
-const envFile = `.env.${process.env.NODE_ENV || "development"}`;
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
 dotenv.config({ path: envFile });
 
-import projectRoutes from "./routes/projectRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
+const [unexpectedRequest, addErrorToRequestLog, globalErrorHandler] = errorHandler();
+import projectRoutes from './routes/projectRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app: express.Application = express();
 const PORT: string | number = process.env.PORT || 3000;
-
 const httpServer = createServer(app);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/projects", projectRoutes);
-app.use("/auth", authRoutes);
+// Routes
+app.use('/auth', authRoutes);
+app.use('/projects', projectRoutes);
+
+// Error handling middleware
+app.use(unexpectedRequest);
+app.use(addErrorToRequestLog);
+app.use(globalErrorHandler);
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
